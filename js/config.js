69fffbfbc4eadfd7b21c8a8886554ff2b7c36d23
Inge La Rivière <3697652+zoot-inge@users.noreply.github.com>
2026-09-01 09:26:35 +0200

    harmonisatie met template

--- js/config.js
+++ js/config.js
@@ -1,33 +1,32 @@
 //-------------------------------------------------------------------------------------
 //-- File. . . :  config.js
 //-- Bevat . . :  Template voor de  configuratie voor respec
-//--              Gebaseerd op https://github.com/Geonovum/respec/wiki
-//--              Deze file moet worden neergezet in de root-directory van de
-//--              betreffende standaard.
-//-- Door. . . :  Jan van Gelder
 //-------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------
-//-- Log . . . :  20181015 - JvG - Initiele versie
-//-------------------------------------------------------------------------------------
-
+//-- 20181015 - JvG - Initiele versie
+//-- 20260901 - ILaR - generieke actuele respec
 //-------------------------------------------------------------------------------------
 var respecConfig =
-{
-  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "GN-WV",              // Werk Versie
-  //specStatus: "GN-CV",              // Consultatie Versie
-  //specStatus: "VV",              // Vaststellings Versie
-  //specStatus: "DEF",             // Definitieve Versie
-  //specStatus: "LD",             // Living Document
-  //-- specType is verplicht! (activeer 1 van de volgende) ----------------------------
-  //specType: "NO",                   // Norm
-  //specType: "ST",                     // Standaard
+{ 
+  //-- title is verplicht!
+  title: "Informatiemodel Externe Veiligheid versie 4.0.0",
+  //-- specStatus is verplicht! (activeer 1 van de volgende)
+  specStatus: "wv",                 // Werkversie
+  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "def",              // Vastgestelde versie
+  //specStatus: "basis",            // Basis Document
+
+  //-- specType is verplicht bij alle andere dan basis
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
   specType: "IM",                   // Informatie Model
-  //specType: "PR",                   // Praktijk Richtlijn
-  //specType: "HR",                   // HandReiking
-  //specType: "WA",                   // Werkafspraak
-  //specType: "US",                   // Uitwisselings standaard
-  //specType: "PS",                   // Presentatie sstandaard
+  //specType: "PR",                 // Praktijkrichtlijn
+  //specType: "HR",                 // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
+
   //-- format is verplicht! -----------------------------------------------------------
   //format: "markdown",                 // altijd "markdown"
   //-- publishDate is verplicht -------------------------------------------------------
@@ -37,16 +36,15 @@ var respecConfig =
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: [
-    {
+   {
     name:       "Pieter Bresters",
-	company:    "Geonovum",
+	  company:    "Geonovum",
     companyURL: "https://www.geonovum.nl",
-    }
+   }
   ],
   //-- de namen van de Author(s) ------------------------------------------------------
   //-- vul in: per Author: name:, company:, companyURL: -------------------------------
-   authors:
-  [
+   authors: [
    {
     name:       "Paul Janssen",
     company:    "Geonovum",
@@ -67,9 +65,8 @@ var respecConfig =
     company:    "Geonovum",
     companyURL: "https://www.geonovum.nl"
    },
-
   ],
-  //-- shortName is verplicht! (komt in de URL: kies logische naam) --------------------
+  //-- shortName is verplicht! (komt in de URL: kies logische naam/bestaand pad) --------------------
   shortName: "imev", 	              // Wordt gebruikt in de document URL
   //-- pubDomain is verplicht! (komt in de URL: Activeer 1 van de volgende) ------------
   //pubDomain: "mim", 	              // Metamodel Informatie Modellering
@@ -114,7 +111,7 @@ var respecConfig =
   //  },
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit!
   previousPublishDate: "2025-07-10",    	  // Format is "YYYY-MM-DD"
-  previousMaturity: "GN-DEF",                 // kies 1 van deze 3 regels
+  previousMaturity: "def",                 // kies 1 van deze 3 regels
   //previousMaturity: "GN-CV",                   // kies 1 van deze 3 regels
   //previousMaturity: "GN-VV",  	          // kies 1 van deze 3 regels
 
